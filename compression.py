caa54e4a15ca15bfed7e7f78c1a343aedac6e9e3
Cyan7 <callie847@gmail.com>
2019-12-20 21:09:12 +0100

    created main and deleted compress.py

--- compression.py
+++ compression.py
@@ -29,10 +29,15 @@ def manualCompress(soundVector, coeff, matrixSize):
     V=np.true_divide(np.dot(initialMatrix,np.transpose(initialMatrix)),(len(soundVector)-1)) #standardize data
     eigVals,eigVects = la.eig(V) # compute eigen values and eigen vectors
 
+
 #MAIN
-h, sample = open("speech_8kHz.wav")
-pca, matrix, out = compression.compress(sample,2,4)
-out = out.astype(int)
-print(np.amax(out))
-save("compressed.wav",h,out)
-#compression.manualCompress(sample,4,4)
+def main():
+    h, sample = wav.open("speech_8kHz.wav")
+    pca, matrix, out = compress(sample,2,4)
+    out = out.astype(int)
+    print(np.amax(out))
+    wav.save("compressed.wav",h,out)
+    #compression.manualCompress(sample,4,4)
+
+if name == "main":
+    main()
