a12b48359a8512ed5a31737b11d64db9aad50284
Cyan7 <callie847@gmail.com>
2019-12-21 19:22:08 +0100

    added functions for tests

--- compression.py
+++ compression.py
@@ -6,6 +6,13 @@ import wav
 def getKey(item):
     return item[0]
 
+def reshapeVect(soundVector,matrixSize):
+    vectorSize = len(soundVector)
+    filling = matrixSize - np.mod(vectorSize, matrixSize) #zeros to add in the padded vector
+    paddedVector = np.lib.pad(soundVector, (0, filling), 'constant', constant_values=0)
+    initialMatrix = paddedVector.reshape((len(paddedVector) // matrixSize, matrixSize)) # Reshape soundVector to be a matrix of dimension matrixSize
+    return initialMatrix
+
 def compress(soundVector, coeff, matrixSize):
     #Reshaping soundVector into a matrix
     vectorSize = len(soundVector)
@@ -53,18 +60,44 @@ def manualCompress(soundVector, coeff, matrixSize):
     decompressed = decompressed[0:-filling]
     return compressedMatrix, decompressed
 
+def memorySpace(initialMatrix, compressedMatrix):
+    iSize = np.size(initialMatrix) #initialMatrix.shape[0]*initialMatrix.shape[1]
+    cSize = np.size(compressedMatrix) #compressedMatrix.shape[0]*compressedMatrix.shape[1]
+    return 100*(iSize-cSize)/iSize
+
+def calculateDistortion(initialMatrix,compressedMatrix):
+    iVec = initialMatrix.flatten()
+    cVec = compressedMatrix.flatten()
+    n=0
+    if(len(iVec)<len(cVec)):
+        n = len(iVec)
+    else :
+        n = len(cVec)
+    d=0
+    for i in range (n-1):
+        d+=(iVec[i]-cVec[i])*(iVec[i]-cVec[i])
+    d/=n
+    return d
+
+
 #MAIN
 def main():
     h, sample = wav.open("speech_8kHz.wav")
-    x=2
+    x=0
+    columnsToKeep = 19
+    matrixSize = 190
     if x == 0:
-        pca, matrix, out = compress(sample,64,256)
+        pca, matrix, out = compress(sample,columnsToKeep,matrixSize)
         out = out.astype(int)
         wav.save("compressed.wav",h,out)
+        print("saved ", memorySpace(out,matrix),"% of memory space")
+        print("distortion = ",calculateDistortion(reshapeVect(sample,matrixSize),out))
     else:
-        _,out = manualCompress(sample,64,256)
+        matrix,out = manualCompress(sample,columnsToKeep,matrixSize)
         out = out.astype(int)
-        wav.save("compressed.wav",h,out)
+        wav.save("compressed2.wav",h,out)
+        print("saved ", memorySpace(out,matrix),"% of memory space")
+        print("distortion = ",calculateDistortion(reshapeVect(sample,matrixSize),out))
 
 if __name__ == "__main__":
     main()
